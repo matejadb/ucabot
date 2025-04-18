d6426d16598ba63e081c7f23232a22cb010ee705
matejadb <92729211+matejadb@users.noreply.github.com>
2025-04-18 11:27:45 +0200

    Test JS link

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './styles.css';
+
+const form = document.getElementById('form');
+const mailInput = document.getElementById('mail');
+const titleInput = document.getElementById('title');
+const textInput = document.getElementById('text-area');
+const pictureInput = document.getElementById('pictures');
+const fileInput = document.getElementById('files');
+const submitButton = document.getElementById('submit-button');
+
+submitButton.addEventListener('click', (e) => {
+	e.preventDefault();
+	console.log(mailInput.value, titleInput.value, textInput.value);
+});
